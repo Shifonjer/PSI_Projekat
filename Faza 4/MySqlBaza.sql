62437e6aaa3f292d71efb13f5845c96fd28b2d12
Shifonjer <necaheroj@yahoo.com>
2020-05-29 16:26:55 +0200

    Izmene u bazi
    Male promene u strukturi baze. Dodate inicijalno propustene kolone.

--- Faza 4/MySqlBaza.sql
+++ Faza 4/MySqlBaza.sql
@@ -34,8 +34,9 @@ CREATE TABLE Prodavac
 CREATE TABLE Proizvod
 ( 
 	id_proizvod        integer  NOT NULL AUTO_INCREMENT,
-	naziv              char(18)  NULL ,
-	kolicina           char(18)  NULL ,
+	naziv              varchar(45)  NULL ,
+	opis               varchar(200)  NULL ,
+	kolicina           integer  NULL ,
 	cena               integer  NULL ,
 	id_prodavac        integer  NOT NULL ,
 	CONSTRAINT XPKProizvod PRIMARY KEY  CLUSTERED (id_proizvod ASC),
@@ -47,9 +48,10 @@ CREATE TABLE Proizvod
 
 CREATE TABLE Korpa
 ( 
+	id_korpa           integer  NOT NULL AUTO_INCREMENT,
 	id_korisnik        integer  NOT NULL ,
 	id_proizvod        integer  NOT NULL ,
-	id_korpa           integer  NOT NULL AUTO_INCREMENT,
+	ime_proizvoda      varchar(45)  NULL ,
 	cena               integer  NULL ,
 	is_active          bit  NULL ,
 	CONSTRAINT XPKKorpa PRIMARY KEY  CLUSTERED (id_korpa ASC),
@@ -66,8 +68,9 @@ CREATE TABLE IstorijaKupovine
 	id_istorija        integer  NOT NULL AUTO_INCREMENT,
 	id_korisnik        integer  NOT NULL ,
 	id_proizvod        integer  NOT NULL ,
+	ime_proizvoda      varchar(45)  NULL ,
 	cena               integer  NULL ,
-	datum              datetime  NULL ,
+	datum              date  NULL ,
 	CONSTRAINT XPKIstorijaKupovine PRIMARY KEY  CLUSTERED (id_istorija ASC),
 	CONSTRAINT vlasnik_istorije FOREIGN KEY (id_korisnik) REFERENCES Kupac(id_korisnik)
 		ON DELETE CASCADE
--- Faza 4/baza.sql
+++ Faza 4/baza.sql
@@ -1,15 +1,4 @@
 
-CREATE TABLE [IstorijaKupovine]
-( 
-	[id_istorija]        integer  NOT NULL ,
-	[id_korisnik]        integer  NOT NULL ,
-	[id_proizvod]        integer  NOT NULL ,
-	[cena]               integer  NULL ,
-	[datum]              datetime  NULL ,
-	CONSTRAINT [XPKIstorijaKupovine] PRIMARY KEY  CLUSTERED ([id_istorija] ASC)
-)
-go
-
 CREATE TABLE [Korisnik]
 ( 
 	[id_korisnik]        integer  NOT NULL ,
@@ -22,6 +11,41 @@ CREATE TABLE [Korisnik]
 )
 go
 
+CREATE TABLE [Kupac]
+( 
+	[id_korisnik]        integer  NOT NULL ,
+	CONSTRAINT [XPKKupac] PRIMARY KEY  CLUSTERED ([id_korisnik] ASC),
+	CONSTRAINT [_kupac] FOREIGN KEY ([id_korisnik]) REFERENCES [Korisnik]([id_korisnik])
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
+)
+go
+
+CREATE TABLE [Prodavac]
+( 
+	[id_korisnik]        integer  NOT NULL ,
+	CONSTRAINT [XPKProdavac] PRIMARY KEY  CLUSTERED ([id_korisnik] ASC),
+	CONSTRAINT [_prodavac] FOREIGN KEY ([id_korisnik]) REFERENCES [Korisnik]([id_korisnik])
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
+)
+go
+
+CREATE TABLE [Proizvod]
+( 
+	[id_proizvod]        integer  NOT NULL ,
+	[naziv]              varchar(45)  NULL ,
+	[kolicina]           integer  NULL ,
+	[cena]               integer  NULL ,
+	[id_prodavac]        integer  NOT NULL ,
+	[opis]               varchar(200)  NULL ,
+	CONSTRAINT [XPKProizvod] PRIMARY KEY  CLUSTERED ([id_proizvod] ASC),
+	CONSTRAINT [proizvodjac] FOREIGN KEY ([id_prodavac]) REFERENCES [Prodavac]([id_korisnik])
+		ON DELETE NO ACTION
+		ON UPDATE NO ACTION
+)
+go
+
 CREATE TABLE [Korpa]
 ( 
 	[id_korisnik]        integer  NOT NULL ,
@@ -29,77 +53,30 @@ CREATE TABLE [Korpa]
 	[id_korpa]           integer  NOT NULL ,
 	[cena]               integer  NULL ,
 	[is_active]          bit  NULL ,
-	CONSTRAINT [XPKKorpa] PRIMARY KEY  CLUSTERED ([id_korpa] ASC)
+	[ime_proizvoda]      varchar(45)  NULL ,
+	CONSTRAINT [XPKKorpa] PRIMARY KEY  CLUSTERED ([id_korpa] ASC),
+	CONSTRAINT [vlasnik_korpe] FOREIGN KEY ([id_korisnik]) REFERENCES [Kupac]([id_korisnik])
+		ON DELETE CASCADE,
+	CONSTRAINT [proizvod_u_korpi] FOREIGN KEY ([id_proizvod]) REFERENCES [Proizvod]([id_proizvod])
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
 )
 go
 
-CREATE TABLE [Kupac]
+CREATE TABLE [IstorijaKupovine]
 ( 
+	[id_istorija]        integer  NOT NULL ,
 	[id_korisnik]        integer  NOT NULL ,
-	CONSTRAINT [XPKKupac] PRIMARY KEY  CLUSTERED ([id_korisnik] ASC)
-)
-go
-
-CREATE TABLE [Prodavac]
-( 
-	[id_korisnik]        integer  NOT NULL ,
-	CONSTRAINT [XPKProdavac] PRIMARY KEY  CLUSTERED ([id_korisnik] ASC)
-)
-go
-
-CREATE TABLE [Proizvod]
-( 
 	[id_proizvod]        integer  NOT NULL ,
-	[naziv]              char(18)  NULL ,
-	[kolicina]           char(18)  NULL ,
 	[cena]               integer  NULL ,
-	[id_prodavac]        integer  NOT NULL ,
-	CONSTRAINT [XPKProizvod] PRIMARY KEY  CLUSTERED ([id_proizvod] ASC)
+	[datum]              datetime  NULL ,
+	[ime_proizvoda]      varchar(45)  NULL ,
+	CONSTRAINT [XPKIstorijaKupovine] PRIMARY KEY  CLUSTERED ([id_istorija] ASC),
+	CONSTRAINT [vlasnik_istorije] FOREIGN KEY ([id_korisnik]) REFERENCES [Kupac]([id_korisnik])
+		ON DELETE CASCADE
+		ON UPDATE NO ACTION,
+	CONSTRAINT [proizvod_istorija] FOREIGN KEY ([id_proizvod]) REFERENCES [Proizvod]([id_proizvod])
+		ON DELETE NO ACTION
+		ON UPDATE NO ACTION
 )
-go
-
-
-ALTER TABLE [IstorijaKupovine]
-	ADD CONSTRAINT [vlasnik_istorije] FOREIGN KEY ([id_korisnik]) REFERENCES [Kupac]([id_korisnik])
-		ON DELETE CASCADE
-		ON UPDATE NO ACTION
-go
-
-ALTER TABLE [IstorijaKupovine]
-	ADD CONSTRAINT [proizvod_istorija] FOREIGN KEY ([id_proizvod]) REFERENCES [Proizvod]([id_proizvod])
-		ON DELETE NO ACTION
-		ON UPDATE NO ACTION
-go
-
-
-ALTER TABLE [Korpa]
-	ADD CONSTRAINT [vlasnik_korpe] FOREIGN KEY ([id_korisnik]) REFERENCES [Kupac]([id_korisnik])
-		ON DELETE CASCADE
-go
-
-ALTER TABLE [Korpa]
-	ADD CONSTRAINT [proizvod_u_korpi] FOREIGN KEY ([id_proizvod]) REFERENCES [Proizvod]([id_proizvod])
-		ON DELETE CASCADE
-		ON UPDATE CASCADE
-go
-
-
-ALTER TABLE [Kupac]
-	ADD CONSTRAINT [_kupac] FOREIGN KEY ([id_korisnik]) REFERENCES [Korisnik]([id_korisnik])
-		ON DELETE CASCADE
-		ON UPDATE CASCADE
-go
-
-
-ALTER TABLE [Prodavac]
-	ADD CONSTRAINT [_prodavac] FOREIGN KEY ([id_korisnik]) REFERENCES [Korisnik]([id_korisnik])
-		ON DELETE CASCADE
-		ON UPDATE CASCADE
-go
-
-
-ALTER TABLE [Proizvod]
-	ADD CONSTRAINT [proizvodjac] FOREIGN KEY ([id_prodavac]) REFERENCES [Prodavac]([id_korisnik])
-		ON DELETE NO ACTION
-		ON UPDATE NO ACTION
 go
